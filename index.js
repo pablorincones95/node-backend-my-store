846aa8a383cb2d609695bdcbd6747ae4327f14aa
Pablo Rincones <pablorincones95@gmail.com>
2024-10-02 17:02:43 -0400

    configuracion del cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const {
   errorHandler,
   logError,
@@ -10,10 +11,31 @@ const port = 3000;
 
 app.use(express.json());
 
+// const whiteList = [
+//   'http://localhost:8080',
+//   'http://localhost:4200',
+//   'https://myapp.com',
+//   'http://127.0.0.1:5500',
+// ];
+
+// const options = {
+//   origin: (origin, callback) => {
+//     if (whiteList.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('acceso de cors no permitido'));
+//     }
+//   },
+// };
+
+app.use(cors());
+
 app.use(require('./routes/index.routes'));
 
 app.use(logError);
+
 app.use(boomErrorHandler);
+
 app.use(errorHandler);
 
 app.listen(port, () => {
