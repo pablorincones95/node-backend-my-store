486a7fdc45aff42aedd024614b9801b4eb879a6f
Pablo Rincones <pablorincones95@gmail.com>
2024-10-28 16:05:01 -0400

    crear una migracion

--- db/config.js
+++ db/config.js
@@ -1,20 +1,17 @@
-const { Sequelize } = require('sequelize');
-
 const { config } = require('./../config/config');
-const setupModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUSer);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 
 const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
-  logging: true,
-});
-
-setupModels(sequelize);
-
-// sequelize.sync();
-
-module.exports = sequelize;
+module.exports = {
+  development: {
+    url: URI,
+    dialect: 'mysql',
+  },
+  production: {
+    url: URI,
+    dialect: 'mysql',
+  },
+};
